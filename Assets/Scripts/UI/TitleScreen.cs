68bd0044bd9a680c1d6b6b827685cdb98b365767
Cidney Hamilton <cidney@cidneyhamilton.com>
2020-01-28 18:13:16 -0500

    Add back a title screen.

---
+++ Assets/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Cyborg.Scenes;
+
+namespace ArcticBlast {
+    
+    public class TitleScreen : MonoBehaviour
+    {
+
+	void Update() {
+	    if (Input.GetKeyDown(KeyCode.Space)) {
+		StartGame();
+	    }
+	}
+
+	void StartGame() {
+	    SceneEvents.ChangeScene("Tutorial");
+	}
+    }
+}

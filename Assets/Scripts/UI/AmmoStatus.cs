e1851e1f7948375080b938e320517201a80f4448
Cidney Hamilton <cidney@cidneyhamilton.com>
2020-02-03 19:50:43 -0500

    Reorganize sound files.

--- Assets/Scripts/UI/AmmoStatus.cs
+++ Assets/Scripts/UI/AmmoStatus.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace ArcticBlast.UI {
-
+    
     public class AmmoStatus : MonoBehaviour
     {
-
-		public Text label;
-		public Slider slider;		
-		
-		public void SetValue(int amount) {
-			label.text = amount.ToString();
-			slider.value = amount;
-		}
-
-		void Start() {
-		   SetValue(0);
-		}
-
-		void OnEnable() {
-			Events.OnUpdateAmmo += SetValue;
-		}
-
-		void OnDisable() {
-			Events.OnUpdateAmmo -= SetValue;
-		}
-		
-
+	
+	public Text label;
+	public Slider slider;		
+	
+	public void SetValue(int amount) {
+	    label.text = amount.ToString();
+	    slider.value = amount;
+	}
+	
+	void Start() {
+	    SetValue(0);
+	}
+	
+	void OnEnable() {
+	    Events.OnUpdateAmmo += SetValue;
+	}
+	
+	void OnDisable() {
+	    Events.OnUpdateAmmo -= SetValue;
+	}
+	
+	
     }
     
 }

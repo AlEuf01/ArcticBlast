b020222a2821f4c4a2fb391c78cf72691fda776d
Cidney Hamilton <cidney@cidneyhamilton.com>
2020-02-10 10:12:33 -0500

    Animate clouds.

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArcticBlast {
+    
+    public class Cloud : MonoBehaviour
+    {
+
+	public float Speed = 0.5f;
+	const int MAX_POSITION = 20;
+
+	void Update() {
+	    Move();
+	    DestroyIfOffScreen();
+	}
+
+	void Move() {
+	    transform.Translate(Speed * Time.deltaTime, 0, 0);
+	}
+	
+	void DestroyIfOffScreen() {
+	    if (transform.position.x > MAX_POSITION) {
+		Destroy(gameObject);
+	    }
+	}
+    }
+}
